659b771661830e7f7ec5cf954b9cfa10aaf8a767
yin-lo <yinlo@hotmail.fr>
2024-05-03 09:50:15 +0200

    step 1 and 2 - route GET tasks ok

--- back/app/controllers/task.js
+++ back/app/controllers/task.js
@@ -1,11 +1,12 @@
 const { Task } = require('../models');
 
 const taskController = {
-
-    listTasks: async function (req, res) {
-        // Récupérer la liste des taches
-        // Renvoyer la liste des taches en json
-    }
+  listTasks: async function (req, res) {
+    // Récupérer la liste des taches
+    const tasks = await Task.findAll();
+    // Renvoyer la liste des taches en json
+    res.json(tasks);
+  },
 };
 
 module.exports = taskController;
